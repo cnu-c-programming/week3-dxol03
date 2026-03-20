650127629f4401b772b1b97c390b0288cf5987ca
dxol03 <151935037+dxol03@users.noreply.github.com>
2026-03-20 09:50:18 +0900

    Update 2-2.c

---
+++ code/2-2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+    int tmp = 0;
+    tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
+int main() {
+    int x = 100, y = 200;
+
+    printf("x = %d, y = %d\n",x,y);
+
+    swap(&x,&y);
+
+    printf("x = %d, y = %d\n",x,y);
+
+    return 0;
+
+
+}
