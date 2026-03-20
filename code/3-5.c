e7234883ed2832d7b785cacec61e0e0c1173727f
dxol03 <151935037+dxol03@users.noreply.github.com>
2026-03-20 14:32:22 +0900

    Update 3-5.c

---
+++ code/3-5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+
+void my_sum(char c,int count, ...) {
+    va_list ap;
+    va_start(ap,count);
+    int sum = 0;
+
+    if(c == 'S') {
+        for (int i = 0; i < count; i++) {
+            printf("%s ",(va_arg(ap,char*)));
+        }
+        printf("\n");
+    } else if (c == 'C') {
+        for (int i = 0; i < count; i++) {
+            printf("%c",(va_arg(ap, int)));
+        }
+            printf("\n");
+    } else if (c == 'D') {
+        for (int i = 0; i < count; i++) {
+            sum += va_arg(ap,int);
+        }
+        printf("%d\n",sum);
+    }
+    va_end(ap);
+}
+
+int main() {
+    my_sum('S',2,"Hello","World");
+    my_sum('C',3,'C','N','U');
+    my_sum('D',4,10,20,30,40);
+
+    return 0;
+}
